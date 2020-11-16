efd8b261661418022420fbe6c4d625c64020e2cd
justinguerrero <justin.guerrero@ecat1.montana.edu>
2020-11-16 16:12:41 -0700

    finished all them tests!

--- battlebit/src/server.c
+++ battlebit/src/server.c
@@ -13,13 +13,6 @@
 #include<sys/socket.h>
 #include<arpa/inet.h>    //inet_addr
 #include<unistd.h>    //write
-#include<netdb.h>
-#include<netinet/in.h>
-#include<sys/types.h>
-void *connection_handler(void*);
-int socket_desc, client_sock, c;
-struct sockaddr_in server, client;
-pthread_t thread_id;
 
 static game_server *SERVER;
 
@@ -46,225 +39,156 @@ int handle_client_connect(int player) {
     // This function will end up looking a lot like repl_execute_command, except you will
     // be working against network sockets rather than standard out, and you will need
     // to coordinate turns via the game::status field.
+
+    char raw_buffer[2000];
+    char_buff *input_buffer = cb_create(2000);
+    char_buff *output_buffer = cb_create(2000);
+
+    int read_size;
+    int fd = SERVER->player_sockets[player];
+    cb_append(output_buffer, "\nbattleBit (? for help) > ");
+    cb_write(fd, output_buffer);
+
+
+    while ((read_size = recv(fd, raw_buffer, 2000, 0)) > 0) {
+        cb_reset(output_buffer);
+        cb_reset(input_buffer);
+        if (read_size > 0) {
+            raw_buffer[read_size] = '\0'; //null terminate read
+
+            // append to input buffer
+            cb_append(input_buffer, raw_buffer);
+
+            char *command = cb_tokenize(input_buffer, " \r\n");
+
+            if (command) {
+                char* arg1 = cb_next_token(input_buffer);
+                char* arg2 = cb_next_token(input_buffer);
+
+                if (strcmp(command, "?") == 0) {
+                    // create output
+                    cb_append(output_buffer, "? - show help\n");
+                    cb_append(output_buffer,"load <string> - load a ship layout file for the given player\n");
+                    cb_append(output_buffer,"show - shows the board for the given player\n");
+                    cb_append(output_buffer,"fire [0-7] [0-7] - fire at the given position\n");
+                    cb_append(output_buffer,"say <string> - Send the string to all players as part of a chat\n");
+                    cb_append(output_buffer,"reset - reset the game\n");
+                    cb_append(output_buffer,"server - start the server\n");
+                    cb_append(output_buffer,"exit - quit the server\n");
+                    //cb_append(output_buffer, command);
+                    //out put it
+                    cb_write(fd, output_buffer);
+                } else if (strcmp(command, "exit") == 0) {
+                    cb_append(output_buffer, "Goodbye!\n");
+                    cb_write(fd, output_buffer);
+                    close(fd);
+                } else if (command != NULL) {
+                    cb_append(output_buffer, "Command was : ");
+                    cb_append(output_buffer, command);
+
+                    cb_write(fd, output_buffer);
+                } else if (strcmp(command, "show") == 0) {
+                    repl_print_board(game_get_current(), fd, output_buffer);
+                    cb_write(fd, output_buffer);
+                } else if (strcmp(command, "reset") == 0) {
+                    game_init();
+                } else if (strcmp(command, "load") == 0) {
+                    game_load_board(game_get_current(), fd, arg1);
+                } else if (strcmp(command, "fire") == 0) {
+                    game_fire(game_get_current(), fd, atoi(arg1), atoi(arg2));
+                } else if (strcmp(command, "say") == 0) {
+                    server_broadcast(arg1);
+                } else {
+                    cb_append(output_buffer,"Unknown Command: ");
+                    cb_append(output_buffer, command);
+                    cb_append(output_buffer, "\n");
+                    cb_write(fd, output_buffer);
+                }
+
+                cb_reset(output_buffer);
+                cb_append(output_buffer, "\nbattleBut (? for help) > ");
+                cb_write(fd, output_buffer);
+            }
+        }
+    }
+
 }
 
 void server_broadcast(char_buff *msg) {
     // send message to all players
+    cb_write(SERVER->player_sockets[0], msg);
+    cb_write(SERVER->player_sockets[1], msg);
+
+
+
 }
 
 int run_server() {
-
-    int socket_fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-    if (socket_fd == -1)
-    {
-        printf("Could not create socket\n");
-    }
-
-    struct sockaddr_in connecting_to;
-
-    // google.com
-    connecting_to.sin_addr.s_addr = inet_addr("216.58.193.78");
-    connecting_to.sin_family = AF_INET;
-    // we have to deal with endian-ness
-    connecting_to.sin_port = htons(80);
-
-    //Connect to remote server
-    if (connect(socket_fd ,
-            // we cast here because sockaddr_in
-            // is structurally compatible
-            // with sockaddr so it works out.
-            // Insane, but this is the
-            // recommended approach o_O
-                (const struct sockaddr *) &connecting_to,
-                sizeof(connecting_to)) < 0)
-    {
-        puts("Could not connect!\n");
-    } else {
-        puts("Connected!\n");
-    }
-        while( (client_sock = accept(socket_desc, (struct sockaddr *)&client, (socklen_t*)&c)) ) {
-            printf("Connection accepted\n");
-
-            if (pthread_create(&SERVER->server_thread, NULL, handle_client_connect, (void *) &client_sock) < 0) {
-                printf("Could not create thread\n");
-                return 1;
-            }
-        //Now join the thread , so that we dont terminate before the thread
-            pthread_join( SERVER->server_thread , NULL);
-            printf("Handler assigned\n");
-        }
-
-    //Send some data
-    char * message = "GET / HTTP/1.1\r\n\r\n";
-    if( send(socket_fd , message ,
-             strlen(message) , 0) < 0)
-    {
-        puts("Send failed");
-    } else {
-        puts("Data Sent!\n");
-
-        //Receive some data
-        char buffer[2000];
-        if( recv(socket_fd, buffer , 2000 , 0) < 0)
-        {
-            puts("Recieve failed");
-        } else {
-            puts("Reply received\n");
-            puts(buffer);
-        }
-    }
-}
-//
-//    listen(socket_desc , 3);
-//
-//    //Accept and incoming connection
-//    printf("Waiting for incoming connections...\n");
-//    c = sizeof(struct sockaddr_in);
-//
-//    //Accept and incoming connection
-//    printf("Waiting for incoming connections...\n");
-//    c = sizeof(struct sockaddr_in);
-//
-//    while( (client_sock = accept(socket_desc, (struct sockaddr *)&client, (socklen_t*)&c)) )
-//    {
-//        printf("Connection accepted\n");
-//
-//        if( pthread_create( &SERVER->server_thread , NULL ,  handle_client_connect , (void*) &client_sock) < 0)
-//        {
-//            printf("Could not create thread\n");
-//            return 1;
-//        }
-//
-//        //Now join the thread , so that we dont terminate before the thread
-//        pthread_join( SERVER->player_threads , NULL);
-//        printf("Handler assigned\n");
-//    }
-//
-//    if (client_sock < 0)
-//    {
-//        printf("accept failed\n");
-//        return 1;
-//    }
-
-
     // STEP 8 - implement the server code to put this on the network.
     // Here you will need to initalize a server socket and wait for incoming connections.
-    //
     // When a connection occurs, store the corresponding new client socket in the SERVER.player_sockets array
     // as the corresponding player position.
-    //
     // You will then create a thread running handle_client_connect, passing the player number out
     // so they can interact with the server asynchronously
 
-
-int server_start() {
-//    init_server();
-//    pthread_create(&SERVER->server_thread, NULL, (void *) run_server, &client_sock);
-//
-//}
-//     STEP 7 - using a pthread, run the run_server() function asynchronously, so you can still
-//     interact with the game via the command line REPL
-
-    int server_socket_fd = socket(AF_INET,
-                                  SOCK_STREAM,
-                                  IPPROTO_TCP);
+    // creates socket
+    int server_socket_fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
     if (server_socket_fd == -1) {
-        printf("Could not create socket\n");
+        printf("Can't create socket\n");
     }
 
+    //resuse port
     int yes = 1;
-    setsockopt(server_socket_fd,
-               SOL_SOCKET,
-               SO_REUSEADDR, &yes, sizeof(yes));
+    setsockopt(server_socket_fd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes));
 
     struct sockaddr_in server;
 
-    // fill out the socket information
+    //socket information
     server.sin_family = AF_INET;
-    // bind the socket on all available interfaces
+
+    // bind socket
     server.sin_addr.s_addr = INADDR_ANY;
     server.sin_port = htons(9876);
 
     int request = 0;
-    if (bind(server_socket_fd,
-            // Again with the cast
-             (struct sockaddr *) &server,
-             sizeof(server)) < 0) {
-        puts("Bind failed");
+    if ( bind(server_socket_fd, (struct sockaddr *)&server , sizeof(server)) < 0)
+    {
+        puts("BIND FAILED\n");
     } else {
-        puts("Bind worked!");
-        listen(server_socket_fd, 88);
-
-        //Accept an incoming connection
-        puts("Waiting for incoming connections...");
-
-
-        struct sockaddr_in client;
-        socklen_t size_from_connect;
-        int client_socket_fd;
-        int request_count = 0;
-        while ((client_socket_fd = accept(server_socket_fd,
-                                          (struct sockaddr *) &client,
-                                          &size_from_connect)) > 0) {
-            char raw_buffer[2000];
-            char_buff *input_buffer = cb_create(2000);
-            char_buff *output_buffer = cb_create(2000);
-
-            int read_size;
-            cb_append(output_buffer, "\nbattleBit (? for help) > ");
-            cb_write(client_socket_fd, output_buffer);
-
-            while((read_size = recv(client_socket_fd, raw_buffer, 2000, 0))>0){
-                cb_reset(output_buffer);
-                cb_reset(input_buffer);
-                if(read_size>0){
-                    raw_buffer[read_size] = '\0';
-
-                    cb_append_int(input_buffer, raw_buffer);
-
-                    char *command = cb_tokenize(input_buffer, " \r\n");
-                    if(strcmp(command, "help") == 0){
-                        cb_append(output_buffer, "a useful message here");
-                        cb_append(output_buffer, command);
-                        cb_write(client_socket_fd, output_buffer);
-
-                    }else if(strcmp(command, "quit")==0){
-                        close(client_socket_fd);
-
-                    }else if(command != NULL){
-                        cb_append(output_buffer, "command was : ");
-                        cb_append(output_buffer, command);
-                        cb_write(client_socket_fd, output_buffer);
-                    }
-                    cb_reset(output_buffer);
-                    cb_append(output_buffer, "\nbattleBit (? for help) > ");
-                    cb_write(client_socket_fd, output_buffer);
-                }
-            }
-
-        }
+        puts("BIND WORKED\n");
+        listen(server_socket_fd, 3);
     }
-    //Create server socket
-    socket_desc = socket(AF_INET , SOCK_STREAM , 0);
-    if (socket_desc == -1)
-    {
-        printf("Could not create socket\n");
-        return 0;
-    }
-    printf("Server Socket Created\n");
 
-    //Prepare the sockaddr_in structure
-    server.sin_family = AF_INET;
-    server.sin_addr.s_addr = INADDR_ANY;
-    server.sin_port = htons(8000);
+    puts("WAITING FOR CONNECTION\n");
 
-    //Bind the port to the server socket
-    if( bind(socket_desc,(struct sockaddr *)&server , sizeof(server)) < 0)
-    {
-        printf("bind failed\n");
-        return 0;
+    // client socket descrption
+    struct sockaddr_in client;
+    socklen_t size_from_connect;
+    int client_socket_fd;
+    int request_count = 0;
+
+    while((client_socket_fd = accept(server_socket_fd,
+                                     (struct sockaddr *) &client, &size_from_connect)) > 0) {
+
+        //int opponent = (player + 1) % 2;
+
+        SERVER->player_sockets[request_count] = client_socket_fd;
+
+
+        pthread_create(&SERVER->player_threads[request_count], NULL, handle_client_connect, request_count);
+
+        request_count++;
+
     }
-    printf("bind done\n");
-    return 1;
+
+
 }
 
+int server_start() {
+    // STEP 7 - using a pthread, run the run_server() function asynchronously, so you can still
+    // interact with the game via the command line REPL
+    init_server();
+    pthread_create(&SERVER->server_thread, NULL, run_server, NULL);
+
+
+}
